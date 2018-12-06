1bfcf92ec8e249e7d9d55bb94b903b13dec5741f
zdl-jose <36843644+namestudent@users.noreply.github.com>
2018-12-06 09:50:15 +0800

    Update first method.py

--- code/first method.py
+++ code/first method.py
@@ -1,9 +1,1 @@
-def number(self,nums,target):
-  n=len(nums)
-  for x in range(n）：
-    for j in range(x+1,n):
-        if nums[j] == target - nums[x] :
-            return x,j
-            break
-        else:
-           continue
+
